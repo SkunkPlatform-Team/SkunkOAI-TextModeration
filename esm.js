95a15e87f3351e0bba68e1c0b20a84782c406a63
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2025-05-24 21:15:54 -0300

    Create esm.js

---
+++ esm.js
@@ -0,0 +1,18 @@
+function moderateMessage(message){
+  return fetch('https://sp-api.netlify.app/.netlify/functions/moderate-message', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt_moderation: message
+    }),
+  })
+  .then(res => res.json())
+  .catch(err => {
+    console.error('Error:', err);
+    return { error: true, message: err.message };
+  });
+}
+
+export moderateMessage
