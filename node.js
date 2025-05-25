9c7244643e5d891884a4aa3464609905e5cce33e
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2025-05-24 21:20:58 -0300

    Update node.js

--- node.js
+++ node.js
@@ -3,8 +3,8 @@ const { post } = require('axios');
 async function moderateMessage(message) {
   return post(
     'https://sp-api.netlify.app/.netlify/functions/moderate-message',
-    { prompt_moderation: message }, // cuerpo (body) como objeto
-    { headers: { 'Content-Type': 'application/json' } } // config (headers)
+    { prompt_moderation: message },
+    { headers: { 'Content-Type': 'application/json' } }
   )
   .then(res => res.data)
   .catch(err => {
